5373af61ea37a7429200646718b2e2ab94be0c4e
gotankersley <gotankersley@harding.edu>
2025-02-14 14:45:50 -0600

    example

--- src/players/api-example.js
+++ src/players/api-example.js
@@ -2,24 +2,24 @@
 import { apiPost } from '../lib/network-lib.js';
 import { Board } from '../core/board.js';
 
-const BASE_URL = 'https://softserve.harding.edu/aivai/';
+const BASE_URL = '/battlesheep/api-proxy/';
 
-const MAX_ITERATIONS = 100;
+const MAX_ITERATIONS = 1;
 
 
 const stateUrl = BASE_URL + 'play-state';
 for (var i = 0; i < MAX_ITERATIONS; i++) {
     
-    //Get the state
-    var data = apiPost(stateUrl, {}); //Synchronous API Call
+    //Get the state    
+    var data = apiPost(stateUrl, {}); //Synchronous API Call    
     var uuid = data.uuid;
     var boardStr = data.state;
     
     //Parse the state
     var board = Board.fromString(boardStr);
     
-    //Choose a move
-    board.getMoves();
+    //Choose a move    
+    var moves = board.getMoves();
     if (!moves.length) throw ('No moves available');    
     
     var randMove = moves[Math.floor(Math.random() * moves.length)];	//Random spot	
@@ -31,6 +31,7 @@ for (var i = 0; i < MAX_ITERATIONS; i++) {
         randMove.count + '|' +
         randMove.dst.q + ',' + randMove.dst.r
     );
+    var moveStr = '1,0|2|0,1';
     
     //Submit the move action
     var actionArgs = {uuid: uuid, action: moveStr};
